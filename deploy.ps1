de1d729328f174d069ebb457c87022e38d14c01f
yuto0226 <wayne71112@gmail.com>
2024-09-12 16:04:36 +0800

    Update deploy script

--- deploy.ps1
+++ deploy.ps1
@@ -1,5 +1,6 @@
+Remove-Item .\dist\* -Force
 npm run build
-copy .\dist\index.html .\public\404.html
+Copy-Item .\dist\index.html .\public\404.html
 git add .\public\404.html
 git commit -m "Update 404.html"
 git push
