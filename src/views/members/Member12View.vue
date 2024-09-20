a4b0c401ac689aa0ac9a824855ff5294045b39cd
Stanley <stanley1106chen@gmail.com>
2024-09-20 20:12:07 +0800

    Add member

--- src/views/members/Member12View.vue
+++ src/views/members/Member12View.vue
@@ -14,14 +14,6 @@ const members_info_12 = [
         tags: []
     },
     {
-        image: trueURL("img/member/allen.jpg"),
-        name: "陳彥廷",
-        title: "副社長",
-        ig: "maverick_01_freedom",
-        desc: "電機系學生，能自己完成的就自己做，不忘初心，盡力就好，一切隨緣....",
-        tags: []
-    },
-    {
         image: trueURL("img/member/jerry.jpg"),
         name: "盧佳緯",
         title: "副社長",
@@ -30,23 +22,12 @@ const members_info_12 = [
         tags: []
     },
     {
-        image: trueURL("img/member/stanley.jpg"),
-        name: "陳威翰",
-        title: "財務組長",
-        ig: "stanley_1106_",
-        desc: "資工系學生，喜歡參加各式各樣的活動，努力學習資安中",
-        tags: [
-            "貝斯(新)手",
-            "社花"
-        ]
-    },
-    {
-        image: trueURL("img/member/tty.jpg"),
-        name: "曹延宥",
-        title: "器材組長",
-        ig: "tty_9_28",
-        desc: "電機系妹妹，萌萌噠",
-        tags: ["社草"]
+        image:"",
+        name: "連婉妤",
+        title: "設計組長",
+        ig: "kt._422",
+        desc: "",
+        tags: []
     },
     {
         image: trueURL("img/member/fang.jpg"),
@@ -55,17 +36,6 @@ const members_info_12 = [
         ig: "jerry_0804_",
         desc: "資工系的學生，喜歡一個人躲在角落看書。最近在努力學習各種知識，藉此增加資工領域的能力。以前從來沒有想過會走資工，覺得和同學們格格不入，希望大學四年能夠找到好夥伴。",
         tags: []
-    },
-    {
-        image: trueURL("img/member/yuto.jpg"),
-        name: "羅崧瑋",
-        title: "活動組長",
-        ig: "sw_1a4",
-        desc: "資工系學生，熱衷於研究網頁前後端、電腦底層技術，目前正努力學習資訊安全。",
-        tags: [
-            "弓道",
-            "網頁前後端",
-        ]
     },
     {
         image: trueURL("img/member/hyn.jpg"),
@@ -90,7 +60,45 @@ const members_info_12 = [
         ig: "best_twisted_fate",
         desc: "資工系學生，喜歡睡覺，吃水餃不沾醬油，吃咖哩要拌。",
         tags: []
-    }
+    },
+    {
+        image: trueURL("img/member/stanley.jpg"),
+        name: "陳威翰",
+        title: "財務組長",
+        ig: "stanley_1106_",
+        desc: "資工系學生，喜歡參加各式各樣的活動，努力學習資安中",
+        tags: [
+            "貝斯(新)手",
+            "社花"
+        ]
+    },
+    {
+        image: trueURL("img/member/tty.jpg"),
+        name: "曹延宥",
+        title: "器材組長",
+        ig: "tty_9_28",
+        desc: "電機系妹妹，萌萌噠",
+        tags: ["社草"]
+    },
+    {
+        image: trueURL("img/member/yuto.jpg"),
+        name: "羅崧瑋",
+        title: "活動組長",
+        ig: "sw_1a4",
+        desc: "資工系學生，熱衷於研究網頁前後端、電腦底層技術，目前正努力學習資訊安全。",
+        tags: [
+            "弓道",
+            "網頁前後端",
+        ]
+    },
+    {
+        image: trueURL("img/member/allen.jpg"),
+        name: "陳彥廷",
+        title: "前副社長",
+        ig: "maverick_01_freedom",
+        desc: "電機系學生，能自己完成的就自己做，不忘初心，盡力就好，一切隨緣....",
+        tags: []
+    },
 ];
 
 </script>
